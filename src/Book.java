1dfb5d8a0747478d399bb92995e56e31ff5245d7
nadillustrator <nadin2208@gmail.com>
2022-09-05 12:22:36 +0300

    HW_SP9 done

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,9 @@
 public class Book {
-    String bookName;
-    Author author;
-    int publishingYear;
+    private String bookName;
+    private Author author;
+    private int publishingYear;
 
-    Book(String bookName, Author author, int publishingYear){
+    public Book(String bookName, Author author, int publishingYear){
 
         this.bookName = bookName;
         this.publishingYear = publishingYear;
